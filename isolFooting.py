2564d905a4f6186ac591a38909e8134b3af871c1
Ricardo Corsini <corsini.eng@gmail.com>
2024-02-26 15:53:02 -0300

    ajustes nomes linetypes

--- isolFooting.py
+++ isolFooting.py
@@ -35,12 +35,12 @@ class Footing():
 
         #Text
 
-        doc.styles.new("Title", dxfattribs={"font" : "Arial.ttf"})
-        doc.styles.new("Subtitle", dxfattribs={"font" : "Arial.ttf"})
+        doc.styles.new("Title_01", dxfattribs={"font" : "Arial.ttf"})
+        doc.styles.new("Subtitle_01", dxfattribs={"font" : "Arial.ttf"})
 
-       
 
         #linetypes
+   
 
         for name, desc, pattern in Settings.my_line_types:
             if name not in doc.linetypes:
@@ -54,7 +54,7 @@ class Footing():
         #Layers Creation
                 
         doc.layers.new(name='STRUCT_FOOTING_0', dxfattribs={'color': 3})
-        doc.layers.new(name='STRUCT_FOOTING_1', dxfattribs={'color': 3, 'linetype': 'DASHED_FOOTING'})
+        doc.layers.new(name='STRUCT_FOOTING_1', dxfattribs={'color': 3, 'linetype': 'DASHED'})
         doc.layers.new(name='STRUCT_FOOTING_2', dxfattribs={'color': 3, 'linetype': 'DOTTED'})
         doc.layers.new(name='STRUCT_FOOTING_3', dxfattribs={'color': 5})
 
@@ -63,19 +63,19 @@ class Footing():
         msp.add_text(
                     self.title,
                     height=0.1,
-                    dxfattribs={"style": "Title", 'color': 3}
+                    dxfattribs={"style": "Title_01", 'color': 3}
         ).set_placement((self.origin[0], self.origin[1] + self.sideY + 0.40), align=TextEntityAlignment.LEFT)
 
         msp.add_text(
                     'PLANTA',
                     height=0.05,
-                    dxfattribs={"style": "Subtitle", 'color': 3}
+                    dxfattribs={"style": "Subtitle_01", 'color': 3}
         ).set_placement((self.origin[0], self.origin[1] + self.sideY + 0.30), align=TextEntityAlignment.LEFT)
 
         msp.add_text(
                     str(round(self.sideX * 100, 0)) + ' x ' + str(round(self.sideY * 100, 0)),
                     height=0.05,
-                    dxfattribs={"style": "Subtitle", 'color': 3}
+                    dxfattribs={"style": "Subtitle_01", 'color': 3}
         ).set_placement((self.origin[0], self.origin[1] + self.sideY + 0.20), align=TextEntityAlignment.LEFT)
 
         
@@ -102,15 +102,14 @@ class Footing():
         msp.add_text(
                     '11 N8 %%C 8.0 c/11 C=149',
                     height=0.05,
-                    dxfattribs={"style": "Subtitle", 'color': 3, 'rotation': 90}
+                    dxfattribs={"style": "Subtitle_01", 'color': 3, 'rotation': 90}
         ).set_placement((self.origin[0] + self.sideX + 0.1 + flapLength + 0.025, self.origin[1] + self.sideY / 2), align=TextEntityAlignment.TOP_CENTER)
 
         msp.add_text(
                     '234',
                     height=0.05,
-                    dxfattribs={"style": "Subtitle", 'color': 3, 'rotation': 90}
+                    dxfattribs={"style": "Subtitle_01", 'color': 3, 'rotation': 90}
         ).set_placement((self.origin[0] + self.sideX + 0.1 + flapLength -0.015, self.origin[1] + self.sideY / 2), align=TextEntityAlignment.BOTTOM_CENTER)
-
 
 
         msp.add_line([self.origin[0] + self.cob, self.origin[1] - 0.1], [self.origin[0] + self.cob, self.origin[1] - 0.1 - flapLength], dxfattribs={'layer': 'STRUCT_FOOTING_3',})
@@ -123,7 +122,7 @@ class Footing():
         msp.add_text(
                     'CORTE',
                     height=0.06,
-                    dxfattribs={"style": "Subtitle", 'color': 3}
+                    dxfattribs={"style": "Subtitle_01", 'color': 3}
         ).set_placement((self.origin[0] + self.sideX, self.origin[1] + self.sideY + 0.20), align=TextEntityAlignment.LEFT)
         
 
@@ -131,6 +130,8 @@ class Footing():
         doc.saveas(path)
 
     
+
+
     
     def list_footing():
         for footing in Footing.footing_list:
@@ -146,5 +147,5 @@ sapata2 = Footing('S2', [0, 0], 0.05 ,1.2, 1.2, 0.25, 0.40, 1.75, 0.40, 0.40, 'S
 #Footing.list_footing()
 
 #desenha sapata
-sapata1.add_isolFooting()
+sapata2.add_isolFooting()
 
